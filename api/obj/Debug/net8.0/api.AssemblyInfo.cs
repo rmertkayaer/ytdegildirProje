cefd46a21fe18aa505d82348fa07c41dcec7231a
rmertkayaer <rmertkayaer@hotmail.com>
2024-11-12 16:56:41 +0300

    aaa

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbda1f55266d872bccb021039df58be441942f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8486c8894662e81266d4f5a6352900cfac7d4c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
